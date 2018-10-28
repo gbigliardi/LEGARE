3ab2a96eafc1657c956dc0b919dcbd9f0e5bd165
gbigliardi <gbigliardi@gmail.com>
2018-10-28 09:27:19 +0100

    fix

--- sw.js
+++ sw.js
@@ -7,9 +7,9 @@ self.addEventListener('install', function(e) {
        '/LEGARE/index.js',
        '/LEGARE/sw.js',
        '/LEGARE/assets/images/01.jpg',
-       '/LEGARE/assets/images/LEGARE/assets/images/01.png',
-       '/LEGARE/assets/images/LEGARE/assets/images/hashes.json',
-       '/LEGARE/assets/images/LEGARE/assets/images/logo.png'
+       '/LEGARE/assets/images/cropped-favicon-128x128.png',
+       '/LEGARE/assets/images/hashes.json',
+       '/LEGARE/assets/images/logo.png'
      ]);
    })
  );
