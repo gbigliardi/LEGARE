d2aa89347d1f19d0f751e35683b97ca6259d5c43
gbigliardi <gbigliardi@gmail.com>
2018-11-17 07:49:48 +0100

    rivisto sw

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-"serviceWorker"in navigator&&navigator.serviceWorker.register("sw.js").then(function(){console.log("Service Worker Registered")});
+"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("/sw.js").then(function(){console.log("Service Worker Registered")},function(e){console.log("ServiceWorker registration failed: ",e)})});
